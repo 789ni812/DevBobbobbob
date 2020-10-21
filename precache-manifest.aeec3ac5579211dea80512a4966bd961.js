e488915e54672195ee8eb7842846f873afd837fd
789ni812 <devbobbobbob@gmail.com>
2020-10-21 22:18:08 +0100

    Updates

---
+++ precache-manifest.aeec3ac5579211dea80512a4966bd961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046711fb27a289b633be8955daaa609c",
+    "url": "/Devbobbobbob/index.html"
+  },
+  {
+    "revision": "9726d4826fb6e1f45dc4",
+    "url": "/Devbobbobbob/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1a795a96eb3cb52867b",
+    "url": "/Devbobbobbob/static/js/2.99c21178.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Devbobbobbob/static/js/2.99c21178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9726d4826fb6e1f45dc4",
+    "url": "/Devbobbobbob/static/js/main.a56d1b01.chunk.js"
+  },
+  {
+    "revision": "739099c96bbce285e84e",
+    "url": "/Devbobbobbob/static/js/runtime-main.136d7d36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Devbobbobbob/static/media/logo.5d5d9eef.svg"
+  }
+]);

d31f16b124a298f8e76503772695cefd2335db05
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-09 14:56:15 +0000

    Article components renders without crashing

--- devbobbobbob/devbobbobbob/src/__test__/Article.test.js
+++ devbobbobbob/devbobbobbob/src/__test__/Article.test.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import Article from '../components/Articles/Article';
 
 describe ('<Article />', ()=> {
-
-    it('', () => {
-
+    it('renders <Article /> without crashing', () => {
+        shallow(<Article />);
     });
+
+
+    // it('renders the title of the article', () => {
+    //         expect(wrapper.find("h1").text()).toContain("this is the static mock article title.");
+    //       });
     
     // Article Mocked Render:
     // [] create a static article component (only use HTML) to display a full article
@@ -14,4 +19,4 @@ describe ('<Article />', ()=> {
     //     [] date
     //     [] author
     //     [] tags
-})
+});

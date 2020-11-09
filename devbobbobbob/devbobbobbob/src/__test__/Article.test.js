635e429d791eb53e9a8c12ac2cbfdc3aa300562a
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-09 15:46:10 +0000

    Refactored test to include beforeAll with wrapper

--- devbobbobbob/devbobbobbob/src/__test__/Article.test.js
+++ devbobbobbob/devbobbobbob/src/__test__/Article.test.js
@@ -3,14 +3,26 @@ import { shallow } from "enzyme";
 import Article from '../components/Articles/Article';
 
 describe ('<Article />', ()=> {
+    let wrapper;
+    beforeAll(()=>{
+        //console.log('1 - beforeAll')
+        wrapper = shallow(<Article />);
+    });
+
+
     it('renders <Article /> without crashing', () => {
         shallow(<Article />);
     });
 
 it ('renders Article title', () => {
-    const wrapper = shallow(<Article />);
+    
     const title = (<h1>this is the static mock article title.</h1>);
     expect(wrapper.contains(title)).toEqual(true);
+});
+
+
+it ('render Article description', ()=> {
+    
 })
     // Article Mocked Render:
     // [] create a static article component (only use HTML) to display a full article

2dbbedfb3a9ff3abacd2f2cbf29985ecd013464d
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-09 13:14:57 +0000

    Created Article test

---
+++ devbobbobbob/devbobbobbob/src/__test__/Article.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe ('<Article />', ()=> {
+
+    it('', () => {
+
+    });
+    
+    // Article Mocked Render:
+    // [] create a static article component (only use HTML) to display a full article
+    //     [] title
+    //     [] description
+    //     [] date
+    //     [] author
+    //     [] tags
+})

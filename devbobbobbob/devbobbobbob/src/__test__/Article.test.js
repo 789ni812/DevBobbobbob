b43573c96233f879789834ebf05e584ebc199234
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-16 13:22:35 +0000

    renders data-test title

--- devbobbobbob/devbobbobbob/src/__test__/Article.test.js
+++ devbobbobbob/devbobbobbob/src/__test__/Article.test.js
@@ -3,16 +3,27 @@ import { findByTestAttr } from '../../test/testUtils';
 import Article from '../components/Articles/Article';
 
 describe('Component - Article', ()=>{
-let wrapper;
-beforeEach(()=> {
+    let wrapper;
+    beforeEach(()=> {
     wrapper = shallow(<Article />)
-})
-    test('Article component renders', ()=>{
-        const component = findByTestAttr(wrapper, 'component-article');
-        expect(component.length).toBe(1);
     })
-    describe ('All article properties load', () => {
+    
+    test('Article component renders', ()=>{
+    const component = findByTestAttr(wrapper, 'component-article');
+    expect(component.length).toBe(1);
+    })
 
+    describe ('Article properties load:', () => {
+    test('Title', () =>{
+        const title = findByTestAttr(wrapper, 'articleTitle');
+        expect(title.length).toBe(1);
+    })
+    
+    
+
+
+
+    
     })
 })
 
--- devbobbobbob/devbobbobbob/src/components/Articles/Article.js
+++ devbobbobbob/devbobbobbob/src/components/Articles/Article.js
@@ -1,8 +1,8 @@
 function Article() {
 
 return (
-<div>
-    <h1 data-test="component-article">this is the static mock article title.</h1>
+<div data-test="component-article">
+    <h1 data-test="articleTitle">this is the static mock article title.</h1>
     <div className="articleDescription"><p>this is the static mock description of the article.</p></div>
     <div className="createdDate">Created: 08/11/2020</div>
     <div className="author">Author: Stuart Bradford</div>

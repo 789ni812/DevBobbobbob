4bd3649b77db4323e7d19bd56c44e40c2955c6c6
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-16 13:03:41 +0000

    Refactored Article.test to use check prop types and check for data-test="component-article"

--- devbobbobbob/devbobbobbob/src/__test__/Article.test.js
+++ devbobbobbob/devbobbobbob/src/__test__/Article.test.js
@@ -1,43 +1,65 @@
-import React from "react";
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../test/testUtils';
 import Article from '../components/Articles/Article';
 
-describe ('<Article />', ()=> {
-    let wrapper;
-    beforeAll(()=>{
-        //console.log('1 - beforeAll')
-        wrapper = shallow(<Article />);
-    });
+describe('Component - Article', ()=>{
+let wrapper;
+beforeEach(()=> {
+    wrapper = shallow(<Article />)
+})
+    test('Article component renders', ()=>{
+        const component = findByTestAttr(wrapper, 'component-article');
+        expect(component.length).toBe(1);
+    })
+    describe ('All article properties load', () => {
 
-
-    it('renders <Article /> without crashing', () => {
-        shallow(<Article />);
-    });
-
-it ('renders Article title', () => {
-    
-    const title = (<h1>this is the static mock article title.</h1>);
-    expect(wrapper.contains(title)).toEqual(true);
-});
-
-
-it('renders Article description', ()=> {
-    const description = (<div className="articleDescription"><p>this is the static mock description of the article.</p></div>);
-   expect(wrapper.contains(description)).toEqual(true);
+    })
 })
 
-it('renders Article created date', () =>{
-const createdDate = (<div className="createdDate">Created: 08/11/2020</div>);
-expect(wrapper.contains(createdDate)).toEqual(true);
-});
 
-it('renders Artcile author', () =>{
-const author = (<div className="author">Author: Stuart Bradford</div>);
-expect(wrapper.contains(author)).toEqual(true);
-});
 
-it('renders Article tags', () =>{
-const tags = (<div className="tags">Tags: Developer, tutorial, Article</div>);
-expect(wrapper.contains(tags)).toEqual(true);
-});
-});
+
+
+
+// describe('Original testing', () =>{
+//     describe ('<Article />', ()=> {
+//         let wrapper;
+//         beforeAll(()=>{
+//             wrapper = shallow(<Article />);
+//         });
+    
+    
+//         it('renders <Article /> without crashing', () => {
+//             shallow(<Article />);
+//         });
+    
+//     it ('renders Article title', () => {
+        
+//         const title = (<h1>this is the static mock article title.</h1>);
+//         expect(wrapper.contains(title)).toEqual(true);
+//     });
+    
+    
+//     it('renders Article description', ()=> {
+//         const description = (<div className="articleDescription"><p>this is the static mock description of the article.</p></div>);
+//        expect(wrapper.contains(description)).toEqual(true);
+//     })
+    
+//     it('renders Article created date', () =>{
+//     const createdDate = (<div className="createdDate">Created: 08/11/2020</div>);
+//     expect(wrapper.contains(createdDate)).toEqual(true);
+//     });
+    
+//     it('renders Artcile author', () =>{
+//     const author = (<div className="author">Author: Stuart Bradford</div>);
+//     expect(wrapper.contains(author)).toEqual(true);
+//     });
+    
+//     it('renders Article tags', () =>{
+//     const tags = (<div className="tags">Tags: Developer, tutorial, Article</div>);
+//     expect(wrapper.contains(tags)).toEqual(true);
+//     });
+// });
+
+// })
+
--- devbobbobbob/devbobbobbob/src/components/Articles/Article.js
+++ devbobbobbob/devbobbobbob/src/components/Articles/Article.js
@@ -2,7 +2,7 @@ function Article() {
 
 return (
 <div>
-    <h1>this is the static mock article title.</h1>
+    <h1 data-test="component-article">this is the static mock article title.</h1>
     <div className="articleDescription"><p>this is the static mock description of the article.</p></div>
     <div className="createdDate">Created: 08/11/2020</div>
     <div className="author">Author: Stuart Bradford</div>

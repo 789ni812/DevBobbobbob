a22fa9e90a5f7aeddc24200ca6755c7e3abad3a3
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-17 20:00:31 +0000

    Removed obsolete toJson import

--- devbobbobbob/devbobbobbob/src/App.test.js
+++ devbobbobbob/devbobbobbob/src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 import Articles from "./components/Articles/Articles";
-import toJson from "enzyme-to-json";
+
 
 describe('<App />', () => {
   let wrapper;

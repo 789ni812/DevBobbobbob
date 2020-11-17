b9af9f4c617ba6e28083447c6303a25fea935c3b
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-17 20:22:58 +0000

    removed Articles component reference as Articles is not complet yet.

--- devbobbobbob/devbobbobbob/src/App.js
+++ devbobbobbob/devbobbobbob/src/App.js
@@ -1,10 +1,8 @@
-import Articles from './components/Articles/Articles';
-
 function App() {
   return (
     <div data-test="component-app">
         <h1 data-test="site-title">DevBobBobBob</h1>
-        <Articles />
+        
     </div>
   );
 };

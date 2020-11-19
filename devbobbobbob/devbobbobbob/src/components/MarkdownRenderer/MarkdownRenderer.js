b93b096ccbe0bd3560288361d5c20490b0219c8d
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-19 20:56:28 +0000

    First import of MarkdownRenderer from prototype (not wired up yet)

---
+++ devbobbobbob/devbobbobbob/src/components/MarkdownRenderer/MarkdownRenderer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import testMarkdownFile from './testMarkdownFile.md';
+import ReactMarkdown from 'react-markdown';
+
+class MarkdownRenderer extends Component {
+
+  constructor() {
+    super();
+    this.state = { markdown: '' };
+  }
+
+  componentWillMount() {
+    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
+    fetch(testMarkdownFile).then(res => res.text()).then(text => this.setState({ markdown: text }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return <ReactMarkdown source={markdown} />;
+  }
+}
+
+export default MarkdownRenderer;

bf358bd57897f3865588c4f9cff53f54708e8ec8
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-20 14:13:49 +0000

    ArticleNavigation renders list of articles

--- devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.js
+++ devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.js
@@ -1,5 +1,17 @@
-function ArticleNavigation() {
-    return <div data-test="component-article-navigation">articleNavigation loaded</div>
+import React from 'react';
+function ArticleNavigation({articles}) {
+    console.log(articles);
+    return (
+    <div data-test="component-article-navigation">
+    <ul>
+        {
+            articles.map((article) =>
+                <li data-test="list-article">{article.title}</li>
+            )
+        }
+    </ul>
+    </div>
+    )
 }
 
 export default ArticleNavigation;
--- devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.test.js
+++ devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.test.js
@@ -1,12 +1,25 @@
 import { shallow } from 'enzyme';
 import { findByTestAttr } from '../../../test/testUtils';
+
 import ArticleNavigation from './ArticleNavigation';
+import articlesArray from './mockedArticleArray';
+
+
 
 let wrapper;
+
+const setup = () => {
+    return shallow(<ArticleNavigation articles={articlesArray} />)
+}
 describe('Article Navigation', ()=> {
     test('Article Navigation component renders without crashing', () => {
-wrapper = shallow(<ArticleNavigation />)
+wrapper = setup();
         const articleNav = findByTestAttr(wrapper, 'component-article-navigation')
         expect(articleNav.length).toBe(1)
     });
+
+    test('Amount of Articles in the articleArray and articleNaviagtion state are the same', () => {
+        const articles = findByTestAttr(wrapper, 'list-article');
+        expect(articles.length).toBe(articlesArray.length)
+    })
 })

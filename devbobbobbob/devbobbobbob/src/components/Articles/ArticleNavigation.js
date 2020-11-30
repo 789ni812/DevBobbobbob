2337c08b05147396fec2f7eda0d9814af578a1ef
devBob <5435563+789ni812@users.noreply.github.com>
2020-11-30 17:56:30 +0000

    Created basic ArticleNavigation component

--- devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.js
+++ devbobbobbob/devbobbobbob/src/components/Articles/ArticleNavigation.js
@@ -1,42 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-function ArticleNavigation({ articles }) {
-  function ArticleList() {
-    return (
-      <ul>
-        {articles.map((article, key) => (
-          <li data-test="list-article" key={article.id}>
-            <Link to="/article/1">{article.title}</Link>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  function Article() {
-    return <h3>Im an article</h3>;
-  }
-
-  return (
-    <Router>
-      <div data-test="component-article-navigation">
-        <hr />
-        <Link to="/articleList">Article List</Link>
-        <Switch>
-          <Route exact path="/">
-            <ArticleList />
-          </Route>
-          <Route path="/articleList">
-            <ArticleList />
-          </Route>
-          <Route path="/article/1">
-            <Article />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+function ArticleNavigation() {
+  return <h3>component loaded: Article Navigation </h3>;
 }
 
 export default ArticleNavigation;

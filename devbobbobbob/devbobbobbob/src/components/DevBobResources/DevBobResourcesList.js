76805105854d613008faa4fe7227f06aecd1f2f8
devBob <5435563+789ni812@users.noreply.github.com>
2020-12-10 17:28:43 +0000

    Wired resource List - populates resource title in a list

--- devbobbobbob/devbobbobbob/src/components/DevBobResources/DevBobResourcesList.js
+++ devbobbobbob/devbobbobbob/src/components/DevBobResources/DevBobResourcesList.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DevBobResourcesService from "../../services/DevBobResourcesService";
 
 const DevBobResourcesList = () => {
+  const [devBobResources, setDevBobResources] = useState(null);
+
   useEffect(() => {
     retrieveResources();
   }, []);
@@ -9,10 +11,22 @@ const DevBobResourcesList = () => {
   const retrieveResources = () => {
     DevBobResourcesService.getAll().then((response) => {
       console.log("response: ", response);
+      setDevBobResources(response.data.data);
+      console.log("DevBobResources: ", devBobResources);
     });
   };
 
-  return <div>DevBobResourcesList.js loaded</div>;
+  return (
+    <div>
+      DevBobResourcesList.js loaded
+      <ul>
+        {devBobResources &&
+          devBobResources.map((resource, index) => (
+            <li>{resource.attributes.title}</li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default DevBobResourcesList;

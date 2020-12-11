367d9cd104fe6fe5f3fb09f2595e5e145def1759
devBob <5435563+789ni812@users.noreply.github.com>
2020-12-11 13:44:41 +0000

    Background color takes up full page - spacing created around the page

--- devbobbobbob/devbobbobbob/src/App.js
+++ devbobbobbob/devbobbobbob/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MainNavigation from "./components/MainNavigation/MainNavigation";
-
+import styled from "styled-components";
 import "./App.css";
+import { Container } from "@material-ui/core";
+
 function App() {
   return (
     <BrowserRouter>
       <div data-test="component-app" className="App">
-        <h1 data-test="site-title">DevBobBobBob</h1>
-        <MainNavigation data-test="component-mainNavigation" />
+        <Container>
+          <h1 data-test="site-title">DevBobBobBob</h1>
+          <MainNavigation data-test="component-mainNavigation" />
+        </Container>
       </div>
     </BrowserRouter>
   );

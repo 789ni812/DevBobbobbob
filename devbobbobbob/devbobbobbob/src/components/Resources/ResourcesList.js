545e4d57ddf89e875efcbb3abf52dcd80bb79a17
devBob <5435563+789ni812@users.noreply.github.com>
2020-12-16 12:51:59 +0000

    Basic styling with grid layout and Cards from material-ui refactored rendering of resources to be more inline with articles component

--- devbobbobbob/devbobbobbob/src/components/Resources/ResourcesList.js
+++ devbobbobbob/devbobbobbob/src/components/Resources/ResourcesList.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import resourcesService from "../../services/resourcesService";
 
+// Material-ui
+import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
 const Resources = () => {
   const [resources, setResources] = useState([]);
   const [currentResource, setCurrentResource] = useState(null);
@@ -18,42 +27,79 @@ const Resources = () => {
   const setActiveResource = (resource) => {
     setCurrentResource(resource);
   };
-
+  console.log("currentResource", currentResource);
   return (
-    <div>
-      resourcesList.js loaded
-      <ul>
-        {resources &&
-          resources.map((resource) => (
-            <li
-              className="hyperlink"
-              key={resource.id}
-              onClick={() => setActiveResource(resource)}
-            >
-              {resource.attributes.title}
-            </li>
-          ))}
-      </ul>
+    <div style={{ marginTop: 20, padding: 30 }}>
+      <Grid container spacing={4} justify="center">
+        {resources.map((resource) => (
+          <Grid item key={resource.id}>
+            <Card>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {resource.attributes.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  onClick={() => setActiveResource(resource)}
+                >
+                  Read
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
       {currentResource ? (
         <div>
-          Current resource selected
-          <h2>Title: {currentResource.attributes.title}</h2>
+          <p>{currentResource.attributes.title}</p>
           <p>{currentResource.attributes.field_notes}</p>
-          <p>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href={currentResource.attributes.field_external_reference.uri}
-            >
-              {currentResource.attributes.field_external_reference.uri}
-            </a>
-          </p>
         </div>
       ) : (
-        <div>Click a resource for more info...</div>
+        <div>
+          <p>Wot no resource selected.</p>
+        </div>
       )}
     </div>
   );
 };
 
 export default Resources;
+
+{
+  /* <ul>
+              {resources.map((resource) => (
+                <li
+                  className="hyperlink"
+                  key={resource.id}
+                  onClick={() => setActiveResource(resource)}
+                >
+                  {resource.attributes.title}
+                </li>
+              ))}
+            </ul>
+            {currentResource ? (
+              <div>
+                Current resource selected
+                <h2>Title: {currentResource.attributes.title}</h2>
+                <p>{currentResource.attributes.field_notes}</p>
+                <p>
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href={
+                      
+                    }
+                  >
+                    {currentResource.attributes.field_external_reference.uri}
+                  </a>
+                </p>
+              </div>
+            ) : (
+              <div>Click a resource for more info...</div>
+            )} */
+}
